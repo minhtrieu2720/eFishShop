05d4f636cf227f2312399ea5367a19e479183239
trieu nguyen <minhtrieu2720@outlook.com>
2021-02-08 22:58:36 +0700

    add language

---
+++ eFishShop/LocalizationResources/ViewLocalizationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFishShop.LocalizationResources
+{
+    public class ViewLocalizationResource
+    {
+    }
+}
